fd7cf89b382ef8cbf459aaf833df8ad949829a50
Kian Bazarjani <kian@bazza.dev>
2024-10-25 14:24:05 -0400

    Progress

--- apps/sync/src/app.ts
+++ apps/sync/src/app.ts
@@ -7,24 +7,34 @@ import { logger } from 'hono/logger'
 export const { upgradeWebSocket, websocket } =
   createBunWebSocket<ServerWebSocket>()
 
+const upgradeWs = () =>
+  upgradeWebSocket((c) => {
+    const roomSlug = c.req.query('roomSlug')
+
+    return {
+      onOpen(event, ws) {
+        console.log('[OPEN]', roomSlug)
+        ws.send(`Hello from server! ${roomSlug}`)
+      },
+      onClose: () => {
+        console.log('[CLOSED]', roomSlug)
+      },
+    }
+  })
+
 export const app = new Hono()
   .use('*', cors())
   .use(logger())
-  .get(
-    '/ws',
-    upgradeWebSocket((c) => {
-      const roomSlug = c.req.query('roomSlug')
+  .get('/ws', async (c, next) => {
+    // Perform auth and authorization checks
+    console.log('Checking connection...')
 
-      return {
-        onOpen(event, ws) {
-          console.log('[OPEN]', roomSlug)
-          ws.send(`Hello from server! ${roomSlug}`)
-        },
-        onClose: () => {
-          console.log('[CLOSED]', roomSlug)
-        },
-      }
-    }),
-  )
+    await new Promise((resolve) => {
+      setTimeout(resolve, 4000)
+    })
+
+    return next()
+  })
+  .get('/ws', upgradeWs())
 
 export type AppType = typeof app
