46f19a4b55e35b954e31c8abb60e37c3ca263d54
Kian Bazza <kian@bazza.dev>
2024-10-27 12:02:20 -0400

    feat(editor): use custom monaco themes (#37)
    Use custom themes

--- apps/web/components/editor/editor.tsx
+++ apps/web/components/editor/editor.tsx
@@ -1,12 +1,12 @@
-// components/Editor.tsx
-
 'use client'
 
-import { useRef, useState, useCallback } from 'react'
-import { default as MonacoEditor } from '@monaco-editor/react'
+import { useRef, useState, useCallback, useEffect } from 'react'
+import { Monaco, default as MonacoEditor } from '@monaco-editor/react'
 import { editor } from 'monaco-editor'
 import { useCodeRoom } from '@/providers/code-room-provider'
 import { useMonacoBinding } from '@/hooks/use-monaco-binding'
+import { jetbrainsMono } from '@/lib/fonts'
+import { themes } from './themes'
 
 interface EditorProps {
   value?: string
@@ -23,9 +23,15 @@ export default function Editor({
   const [editorMounted, setEditorMounted] = useState(false)
 
   const setupEditor = useCallback(
-    (editorInstance: editor.IStandaloneCodeEditor) => {
+    (editorInstance: editor.IStandaloneCodeEditor, monacoRef: Monaco) => {
       editorInstance.focus()
       editorRef.current = editorInstance
+
+      monacoRef.editor.defineTheme('avelin-dark', themes.dark)
+      monacoRef.editor.defineTheme('avelin-light', themes.light)
+
+      monacoRef.editor.setTheme('avelin-light')
+
       setEditorMounted(true)
     },
     [],
@@ -37,17 +43,26 @@ export default function Editor({
     networkProvider,
   )
 
+  useEffect(() => {
+    if (!!networkProvider) {
+      networkProvider.awareness?.on('change', console.log)
+      networkProvider.awareness?.setLocalStateField('user', 'hello')
+    }
+  }, [])
+
   return (
     <MonacoEditor
       width='100vw'
       height='100vh'
       theme='light'
+      loading={null}
       language={language}
       value={value}
       onMount={setupEditor}
       options={{
         padding: { top: 30, bottom: 30 },
         fontSize: 16,
+        fontFamily: `${jetbrainsMono.style.fontFamily}`,
         fontLigatures: true,
         minimap: { enabled: false },
         renderLineHighlight: 'none',
---
+++ apps/web/components/editor/themes.ts
@@ -0,0 +1,21 @@
+import { editor } from 'monaco-editor'
+import { gray } from '@avelin/ui/colors'
+
+export const themes: Record<string, editor.IStandaloneThemeData> = {
+  dark: {
+    base: 'vs-dark',
+    inherit: true,
+    rules: [],
+    colors: {
+      'editor.background': gray.gray12,
+    },
+  },
+  light: {
+    base: 'vs',
+    inherit: true,
+    rules: [],
+    colors: {
+      'editor.background': gray.gray1,
+    },
+  },
+} as const
