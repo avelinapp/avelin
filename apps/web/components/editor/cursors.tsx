4505b07bebf03b45828e7c892832ff5af745869d
Kian Bazarjani <kian@bazza.dev>
2024-10-28 07:44:20 -0400

    Fade remote cursors out after 3s

--- apps/web/components/editor/cursors.tsx
+++ apps/web/components/editor/cursors.tsx
@@ -1,5 +1,5 @@
 import { assignOption, colors, generateUniqueName } from '@/lib/rooms'
-import { AwarenessList, UserAwareness } from '@/lib/sync'
+import { AwarenessList, USER_IDLE_TIMEOUT, UserAwareness } from '@/lib/sync'
 import { HocuspocusProvider } from '@hocuspocus/provider'
 import { useEffect, useMemo, useState } from 'react'
 
@@ -9,36 +9,68 @@ type CursorsProps = {
 
 export function Cursors({ provider }: CursorsProps) {
   const [awarenessUsers, setAwarenessUsers] = useState<AwarenessList>([])
+  const [tick, setTick] = useState(false)
 
   useEffect(() => {
     const assignedColors = Array.from(
       provider.awareness!.getStates().values(),
     ).map(({ user }) => user?.color)
 
-    console.log(assignedColors)
-
     const color = assignOption(Object.values(colors), assignedColors)
 
     const localUser: UserAwareness['user'] = {
       name: generateUniqueName(),
       color: color,
+      lastActive: Date.now(),
     }
 
     provider.awareness?.setLocalStateField('user', localUser)
 
     // On changes, update `awarenessUsers`
-    function setUsers() {
-      console.log(provider.awareness!.getStates())
-      setAwarenessUsers([...provider.awareness!.getStates()] as AwarenessList)
+    function setUsers({
+      added,
+      updated,
+      // removed,
+    }: {
+      added: number[]
+      updated: number[]
+      // removed: number[]
+    }) {
+      const newAwarenessUsers = (
+        [...provider.awareness!.getStates()] as AwarenessList
+      ).map(([clientId, client]: [number, UserAwareness]) => {
+        if (
+          (added.includes(clientId) || updated.includes(clientId)) &&
+          !!client.user
+        ) {
+          // Update lastActive timestamp
+          return [
+            clientId,
+            {
+              user: { ...client.user, lastActive: Date.now() },
+            },
+          ] satisfies [number, UserAwareness]
+        } else {
+          return [clientId, client] satisfies [number, UserAwareness]
+        }
+      })
+
+      setAwarenessUsers(newAwarenessUsers)
     }
 
     provider.awareness!.on('change', setUsers)
-    setUsers()
 
     return () => {
       provider.awareness!.off('change', setUsers)
     }
   }, [provider])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTick((t) => !t)
+    }, USER_IDLE_TIMEOUT)
+    return () => clearInterval(interval)
+  }, [])
 
   // Insert awareness info into cursors with styles
   const styleSheet = useMemo(() => {
@@ -46,6 +78,8 @@ export function Cursors({ provider }: CursorsProps) {
 
     for (const [clientId, client] of awarenessUsers) {
       if (client?.user) {
+        const isActive = Date.now() - client.user.lastActive < USER_IDLE_TIMEOUT
+
         cursorStyles += `
           .yRemoteSelection-${clientId}, 
           .yRemoteSelectionHead-${clientId}  {
@@ -54,21 +88,20 @@ export function Cursors({ provider }: CursorsProps) {
 
           .yRemoteSelectionHead-${clientId}::after  {
             content: "${client.user.name}";
+            --name-opacity: ${isActive ? 1 : 0};
+            --name-visibility: ${isActive ? 'visible' : 'hidden'};
           }
 
           .yRemoteSelection-${clientId} {
             background-color: ${client.user.color};
           }
-
-          
         `
       }
     }
 
-    console.log(cursorStyles)
-
     return { __html: cursorStyles }
-  }, [awarenessUsers])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [awarenessUsers, tick])
 
   return <style dangerouslySetInnerHTML={styleSheet} />
 }
--- apps/web/lib/sync.ts
+++ apps/web/lib/sync.ts
@@ -1,6 +1,7 @@
 export type UserInfo = {
   name: string
   color: string
+  lastActive: number
   // picture: string
 }
 
@@ -9,3 +10,5 @@ export type UserAwareness = {
 }
 
 export type AwarenessList = Array<[number, UserAwareness]>
+
+export const USER_IDLE_TIMEOUT = 3000 as const // Time to wait before setting user as inactive
