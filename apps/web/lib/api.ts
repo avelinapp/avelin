96308e520be4e30e00e7418ce253966ae73b3f35
Kian Bazarjani <kian@bazza.dev>
2024-11-01 15:46:57 -0400

    build: setup ci/cd pipeline
    build: fix ci/cd canary action
    
    fix: missing type
    
    Add build args to canary workflow
    
    build(gh-actions): update gh action template versions to latest
    
    build(cd): argo cd should target git tags
    
    build: drop canary channels, use v0.*.*
    
    build(ci): handle no semver tags case
    
    Reset version # back to 0
    
    Add release-it
    
    Progress
    
    Test
    
    Test
    
    Test
    
    Fixes
    
    v0.1.1
    
    Reset version number, use minor release by default
    
    Test
    
    Test
    
    Drop github from CI
    
    Test
    
    Progress
    
    Test
    
    Cleanup
    
    Add more scripts
    
    Updates
    
    Progress
    
    Git pull before anything
    
    chore: use auto-changelog
    
    Use conventional commits
    
    Infer next version
    
    PR
    
    Create PR
    
    Do not create new branch

--- apps/web/lib/api.ts
+++ apps/web/lib/api.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@avelin/api/client'
 
 if (!process.env.NEXT_PUBLIC_API_URL) {
-  throw new Error('API_URL is not set')
+  throw new Error('NEXT_PUBLIC_API_URL is not set')
 }
 
 export const api = createClient(process.env.NEXT_PUBLIC_API_URL)
