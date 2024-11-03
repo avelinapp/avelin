14335f590679e3ae3bbf015f7c26ec62e90eac97
Kian Bazarjani <kian@bazza.dev>
2024-11-03 18:47:20 -0500

    Add type for AwarenessChange

--- apps/web/lib/sync.ts
+++ apps/web/lib/sync.ts
@@ -12,3 +12,9 @@ export type UserAwareness = {
 export type AwarenessList = Array<[number, UserAwareness]>
 
 export const USER_IDLE_TIMEOUT = 3000 as const // Time to wait before setting user as inactive
+
+export type AwarenessChange = {
+  added: number[]
+  updated: number[]
+  removed: number[]
+}
