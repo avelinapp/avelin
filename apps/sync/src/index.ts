1776d0f25fa67c703b873c3b2de6db29a7d5c971
Kian Bazarjani <kian@bazza.dev>
2024-11-03 11:21:30 -0500

    Remove unused code, unused npm packages

--- apps/sync/src/index.ts
+++ apps/sync/src/index.ts
@@ -1,4 +1,3 @@
-// import { validateSession } from '@avelin/auth'
 import { Logger } from '@hocuspocus/extension-logger'
 import { Hocuspocus } from '@hocuspocus/server'
 import dotenv from 'dotenv'
@@ -7,42 +6,11 @@ import expressWebsockets from 'express-ws'
 
 dotenv.config()
 
+const port = process.env.API_PORT || 4100
+
 const server = new Hocuspocus({
   port: 4100,
   extensions: [new Logger()],
-  // async onConnect(data) {
-  //   console.log('Connected')
-  //   console.log('Requires auth:', data.connection.requiresAuthentication)
-  //   console.log('Is authenticated:', data.connection.isAuthenticated)
-  //
-  //   const { requiresAuthentication, isAuthenticated } = data.connection
-  //   console.log('context:', data.context.session)
-  //
-  //   if (requiresAuthentication && !isAuthenticated) {
-  //     throw new Error('Terminating connection due to missing authentication.')
-  //   }
-  // },
-  // async onConnect(data) {
-  //   data.connection.requiresAuthentication = true
-  //   console.log('Connected')
-  //   console.log('Connected auth:', data.context.auth)
-  // },
-  // async onAuthenticate(data) {
-  //   console.log('here!')
-  //   const { token: sessionId } = data
-  //   console.log('Session ID:', sessionId)
-  //   const auth = await validateSession(sessionId)
-  //
-  //   console.log('Auth:', auth)
-  //
-  //   if (!auth) {
-  //     throw new Error('Invalid session.')
-  //   }
-  //
-  //   console.log('Authenticated!')
-  //
-  //   return auth
-  // },
 })
 
 const { app } = expressWebsockets(express())
@@ -51,6 +19,6 @@ app.ws('/', (websocket, request) => {
   server.handleConnection(websocket, request)
 })
 
-app.listen(process.env.API_PORT || 4100, () => {
-  console.log(`Listening on port ${process.env.API_PORT || 4100}`)
+app.listen(port, () => {
+  console.log(`Avelin Sync listening on port ${port}`)
 })
