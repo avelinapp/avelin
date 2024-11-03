0f592eb17a75e1064332b26f3f14fad179dbfb73
Kian Bazza <kian@bazza.dev>
2024-11-03 12:19:44 -0500

    infra(sync): switch to node v20 + express v5 (#67)
    * Initial progress, dev server working
    
    * Remove unused code, unused npm packages
    
    * Fix dockerignore only ignoring root items
    
    * Remove unused compile task for sync
    
    * Switch base TS config to base (not hono.json)
    
    * Adapt Dockerfile for node deploy

--- apps/sync/src/index.ts
+++ apps/sync/src/index.ts
@@ -1,43 +1,24 @@
-// import { validateSession } from '@avelin/auth'
 import { Logger } from '@hocuspocus/extension-logger'
 import { Hocuspocus } from '@hocuspocus/server'
+import dotenv from 'dotenv'
+import express from 'express'
+import expressWebsockets from 'express-ws'
+
+dotenv.config()
+
+const port = process.env.API_PORT || 4100
 
 const server = new Hocuspocus({
   port: 4100,
   extensions: [new Logger()],
-  // async onConnect(data) {
-  //   console.log('Connected')
-  //   console.log('Requires auth:', data.connection.requiresAuthentication)
-  //   console.log('Is authenticated:', data.connection.isAuthenticated)
-  //
-  //   const { requiresAuthentication, isAuthenticated } = data.connection
-  //   console.log('context:', data.context.session)
-  //
-  //   if (requiresAuthentication && !isAuthenticated) {
-  //     throw new Error('Terminating connection due to missing authentication.')
-  //   }
-  // },
-  // async onConnect(data) {
-  //   data.connection.requiresAuthentication = true
-  //   console.log('Connected')
-  //   console.log('Connected auth:', data.context.auth)
-  // },
-  // async onAuthenticate(data) {
-  //   console.log('here!')
-  //   const { token: sessionId } = data
-  //   console.log('Session ID:', sessionId)
-  //   const auth = await validateSession(sessionId)
-  //
-  //   console.log('Auth:', auth)
-  //
-  //   if (!auth) {
-  //     throw new Error('Invalid session.')
-  //   }
-  //
-  //   console.log('Authenticated!')
-  //
-  //   return auth
-  // },
 })
 
-server.listen()
+const { app } = expressWebsockets(express())
+
+app.ws('/', (websocket, request) => {
+  server.handleConnection(websocket, request)
+})
+
+app.listen(port, () => {
+  console.log(`Avelin Sync listening on port ${port}`)
+})
