0362ed842149bd41a743727455baf8720119ad03
Kian Bazarjani <kian@bazza.dev>
2024-11-04 20:19:35 -0500

    Drop unused logic, add avelin logo and separator to left side of toolbar

--- apps/web/components/editor/editor-toolbar.tsx
+++ apps/web/components/editor/editor-toolbar.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-// import { Input } from '@avelin/ui/input'
 import { Button } from '@avelin/ui/button'
-import { CopyIcon, LinkIcon } from '@avelin/icons'
+import { Separator } from '@avelin/ui/separator'
+import { CopyIcon, LinkIcon, LogoAvelin } from '@avelin/icons'
 import { EditorLanguageCombobox } from './editor-language-combobox'
 import { UsersList } from './editor-users-list'
 import { useCodeRoom } from '@/providers/code-room-provider'
@@ -12,13 +12,12 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from '@avelin/ui/tooltip'
-import { useMemo, useState } from 'react'
+import { useMemo } from 'react'
 import { toast } from '@avelin/ui/sonner'
 import { useCopyToClipboard } from '@avelin/ui/hooks'
 
 function CopyRoomURL({ roomSlug }: { roomSlug: string }) {
   const [, copy] = useCopyToClipboard()
-  const [copied, setCopied] = useState(false)
 
   const roomUrl = useMemo(
     () => process.env.NEXT_PUBLIC_APP_URL + '/' + roomSlug,
@@ -27,8 +26,6 @@ function CopyRoomURL({ roomSlug }: { roomSlug: string }) {
 
   function handleCopy(notify?: boolean) {
     copy(roomUrl)
-
-    setCopied(true)
 
     if (notify) {
       toast('Room link copied to your clipboard - share it!', {
@@ -45,10 +42,6 @@ function CopyRoomURL({ roomSlug }: { roomSlug: string }) {
         ),
       })
     }
-
-    setTimeout(() => {
-      setCopied(false)
-    }, 2000)
   }
 
   return (
@@ -71,7 +64,7 @@ function CopyRoomURL({ roomSlug }: { roomSlug: string }) {
           className='text-xs'
           align='end'
         >
-          Copy room URL
+          Copy room link
         </TooltipContent>
       </Tooltip>
     </TooltipProvider>
@@ -83,8 +76,17 @@ export function EditorToolbar() {
   return (
     <div className='flex items-center m-2 drop-shadow-sm py-2 px-2 max-w-full bg-white rounded-lg border border-color-border-subtle'>
       <div className='w-full grid grid-cols-3'>
-        <div className='flex items-center gap-4 place-self-start'>
-          <EditorLanguageCombobox />
+        <div className='flex items-center place-self-start'>
+          <div className='flex items-center h-6'>
+            <LogoAvelin className='size-6 shrink-0' />
+            <Separator
+              className='mx-3'
+              orientation='vertical'
+            />
+          </div>
+          <div className='flex items-center gap-4'>
+            <EditorLanguageCombobox />
+          </div>
         </div>
         <div className='place-self-center'>
           {/* <Input */}
