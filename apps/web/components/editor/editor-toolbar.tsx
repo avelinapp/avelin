cbd343c1c5411c8dbe779323a5d0f55597d96c7c
Kian Bazarjani <kian@bazza.dev>
2024-11-04 19:29:20 -0500

    Add share button to copy room link

--- apps/web/components/editor/editor-toolbar.tsx
+++ apps/web/components/editor/editor-toolbar.tsx
@@ -1,36 +1,90 @@
 'use client'
 
-// eslint-disable-next-line
-import { Input } from '@avelin/ui/input'
+// import { Input } from '@avelin/ui/input'
 import { Button } from '@avelin/ui/button'
-import { CopyIcon } from '@avelin/icons'
+import { CopyIcon, LinkIcon } from '@avelin/icons'
 import { EditorLanguageCombobox } from './editor-language-combobox'
 import { UsersList } from './editor-users-list'
+import { useCodeRoom } from '@/providers/code-room-provider'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@avelin/ui/tooltip'
+import { useMemo, useState } from 'react'
+import { toast } from '@avelin/ui/sonner'
+import { useCopyToClipboard } from '@avelin/ui/hooks'
 
-// eslint-disable-next-line
-function CopyRoomURL() {
+function CopyRoomURL({ roomSlug }: { roomSlug: string }) {
+  const [, copy] = useCopyToClipboard()
+  const [copied, setCopied] = useState(false)
+
+  const roomUrl = useMemo(
+    () => process.env.NEXT_PUBLIC_APP_URL + '/' + roomSlug,
+    [roomSlug],
+  )
+
+  function handleCopy(notify?: boolean) {
+    copy(roomUrl)
+
+    setCopied(true)
+
+    if (notify) {
+      toast('Room link copied to your clipboard - share it!', {
+        description: roomUrl,
+        action: (
+          <Button
+            size='xs'
+            variant='ghost'
+            className='p-1.5 h-fit rounded-md ml-auto'
+            onClick={() => handleCopy(false)}
+          >
+            <CopyIcon className='size-4 shrink-0' />
+          </Button>
+        ),
+      })
+    }
+
+    setTimeout(() => {
+      setCopied(false)
+    }, 2000)
+  }
+
   return (
-    <Button
-      variant='secondary'
-      size='xs'
-      className='text-secondary-text'
-    >
-      <CopyIcon
-        className='size-4 shrink-0 text-color-text-secondary'
-        strokeWidth={2.25}
-      />
-      Copy URL
-    </Button>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            variant='default'
+            size='xs'
+            onClick={() => handleCopy(true)}
+          >
+            <LinkIcon
+              className='size-4 shrink-0'
+              strokeWidth={2.25}
+            />
+            Share
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent
+          className='text-xs'
+          align='end'
+        >
+          Copy room URL
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   )
 }
 
 export function EditorToolbar() {
+  const { room } = useCodeRoom()
   return (
     <div className='flex items-center m-2 drop-shadow-sm py-2 px-2 max-w-full bg-white rounded-lg border border-color-border-subtle'>
       <div className='w-full grid grid-cols-3'>
         <div className='flex items-center gap-4 place-self-start'>
-          <UsersList />
-          {/* <CopyRoomURL /> */}
+          <EditorLanguageCombobox />
         </div>
         <div className='place-self-center'>
           {/* <Input */}
@@ -38,8 +92,9 @@ export function EditorToolbar() {
           {/*   className='font-medium' */}
           {/* /> */}
         </div>
-        <div className='place-self-end'>
-          <EditorLanguageCombobox />
+        <div className='place-self-end flex items-center gap-1'>
+          <UsersList />
+          <CopyRoomURL roomSlug={room?.slug ?? ''} />
         </div>
       </div>
     </div>
