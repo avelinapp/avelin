942f1221752aff732f8ec6db2f851e9fba22b44e
Kian Bazarjani <kian@bazza.dev>
2024-11-06 00:23:31 -0500

    Progress

---
+++ apps/web/components/editor/copy-room-url.tsx
@@ -0,0 +1,68 @@
+'use client'
+
+import { Button } from '@avelin/ui/button'
+import { CopyIcon, LinkIcon } from '@avelin/icons'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@avelin/ui/tooltip'
+import { useMemo } from 'react'
+import { toast } from '@avelin/ui/sonner'
+import { useCopyToClipboard } from '@avelin/ui/hooks'
+
+export function CopyRoomURL({ roomSlug }: { roomSlug: string }) {
+  const [, copy] = useCopyToClipboard()
+
+  const roomUrl = useMemo(
+    () => process.env.NEXT_PUBLIC_APP_URL + '/' + roomSlug,
+    [roomSlug],
+  )
+
+  function handleCopy(notify?: boolean) {
+    copy(roomUrl)
+
+    if (notify) {
+      toast('Room link copied to your clipboard - share it!', {
+        description: roomUrl,
+        action: (
+          <Button
+            size='xs'
+            variant='ghost'
+            className='p-1.5 h-fit rounded-md ml-auto'
+            onClick={() => handleCopy(false)}
+          >
+            <CopyIcon className='size-4 shrink-0' />
+          </Button>
+        ),
+      })
+    }
+  }
+
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            variant='default'
+            size='xs'
+            onClick={() => handleCopy(true)}
+          >
+            <LinkIcon
+              className='size-4 shrink-0'
+              strokeWidth={2.25}
+            />
+            Share
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent
+          className='text-xs'
+          align='end'
+        >
+          Copy room link
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
+}
--- apps/web/components/editor/network-status.tsx
+++ apps/web/components/editor/network-status.tsx
@@ -2,20 +2,60 @@
 
 import { useCodeRoom } from '@/providers/code-room-provider'
 import { useNetworkStatus } from '@avelin/ui/hooks'
+import { Badge } from '@avelin/ui/badge'
+import { ZapIcon } from '@avelin/icons'
+import { cn } from '@avelin/ui/cn'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@avelin/ui/tooltip'
 
-export function NetworkStatus() {
+// eslint-disable-next-line
+interface NetworkStatusBadgeProps
+  extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function NetworkStatusBadge({ className }: NetworkStatusBadgeProps) {
   const { networkProvider: provider } = useCodeRoom()
   const { isOnline } = useNetworkStatus()
 
   if (!provider) return null
 
-  if (!isOnline) {
-    return <div>Offline</div>
-  }
-
-  if (provider.status !== 'connected') {
-    return <div className='text-green-500'>Sync offline</div>
-  }
-
-  return <div className='text-green-500'>Online</div>
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          {!isOnline ? (
+            <Badge
+              variant='secondary'
+              className={cn(
+                'inline-flex items-center gap-0.5',
+                'animate-in fade-in-5 zoom-in-95 ease-out',
+                className,
+              )}
+            >
+              <ZapIcon className='size-3 shrink-0' />
+              Offline
+            </Badge>
+          ) : provider.status !== 'connected' ? (
+            <Badge
+              variant='secondary'
+              className={cn(
+                'inline-flex items-center gap-0.5',
+                'animate-in fade-in-5 zoom-in-95 ease-out',
+                className,
+              )}
+            >
+              <ZapIcon className='size-3 shrink-0' />
+              Sync offline
+            </Badge>
+          ) : null}
+        </TooltipTrigger>
+        <TooltipContent align='end'>
+          You are currently not connected to the network.
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
 }
