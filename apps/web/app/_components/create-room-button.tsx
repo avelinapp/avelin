ecb67fea4c2e3caa11f5164a03fe16524fac1c68
Kian Bazarjani <kian@bazza.dev>
2024-11-10 17:02:29 -0500

    Add shortcut for creating rooms

--- apps/web/app/_components/create-room-button.tsx
+++ apps/web/app/_components/create-room-button.tsx
@@ -1,10 +1,11 @@
 'use client'
 
 import { Button } from '@avelin/ui/button'
-import { CodeIcon } from '@avelin/icons'
+import { PlusIcon } from '@avelin/icons'
 import { useScramble } from 'use-scramble'
 import { useCreateRoom } from '@/lib/mutations'
 import { useRouter } from 'next/navigation'
+import { useKeyPress } from '@avelin/ui/hooks'
 
 export default function CreateRoomButton() {
   const router = useRouter()
@@ -17,6 +18,8 @@ export default function CreateRoomButton() {
       },
     })
 
+  useKeyPress(['c'], createRoom)
+
   const buttonText = !isPending ? 'Create room' : 'Building...'
   const { ref } = useScramble({
     text: buttonText,
@@ -27,20 +30,27 @@ export default function CreateRoomButton() {
   return (
     <Button
       size='lg'
-      className='bg-indigo-9 hover:bg-indigo-9 text-gray-1 inline-flex items-center min-w-[275px] text-lg'
+      className='bg-indigo-9 hover:bg-indigo-10 group text-indigo-1 inline-flex items-center min-w-[275px] text-lg'
       disabled={isPending}
       onClick={createRoom}
     >
       {!isPending && (
-        <CodeIcon
+        <PlusIcon
           strokeWidth={3}
-          className='h-10 w-10 shrink-0'
+          className='size-24 shrink-0'
         />
       )}
-      <span ref={ref} />
-      <div className='ml-2 font-mono rounded-sm size-7 inline-flex items-center justify-center text-indigo-3 font-normal bg-indigo-11 text-base'>
-        C
-      </div>
+      <span
+        className='text-indigo-2'
+        ref={ref}
+      />
+      {!isPending && <KeyboardShortcut />}
     </Button>
   )
 }
+
+const KeyboardShortcut = () => (
+  <div className='ml-2 font-mono rounded-sm size-7 inline-flex items-center justify-center text-indigo-3 font-normal bg-indigo-12/50 text-base'>
+    C
+  </div>
+)
