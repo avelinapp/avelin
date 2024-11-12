82a52aa2e6c9cad4f71b1a63def2c6e50b4052df
Kian Bazarjani <kian@bazza.dev>
2024-11-12 17:34:11 -0500

    Use logout mutation to logout and redirect to home

--- apps/web/components/editor/my-account-dropdown.tsx
+++ apps/web/components/editor/my-account-dropdown.tsx
@@ -1,5 +1,13 @@
+'use client'
+
+import { useLogout } from '@/lib/mutations'
 import { Auth } from '@avelin/database'
-import { LogOutIcon } from '@avelin/icons'
+import {
+  HouseIcon,
+  KeyRoundIcon,
+  LogOutIcon,
+  SettingsIcon,
+} from '@avelin/icons'
 import { Avatar, AvatarImage, AvatarFallback } from '@avelin/ui/avatar'
 import { Button } from '@avelin/ui/button'
 import { cn } from '@avelin/ui/cn'
@@ -7,12 +15,37 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@avelin/ui/dropdown-menu'
+import { toast } from '@avelin/ui/sonner'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 
 export function MyAccountDropdown({ user }: { user: Auth['user'] }) {
+  const router = useRouter()
+
   const [open, setOpen] = useState(false)
+
+  const logout = useLogout()
+
+  const handleLogout = async () => {
+    logout.mutate(undefined, {
+      onSuccess: () => {
+        toast('Logged out.', {
+          icon: <KeyRoundIcon className='size-4 shrink-0' />,
+        })
+        router.push('/login')
+      },
+      onError: (err) => {
+        toast.error('Failed to log out.', {
+          icon: <KeyRoundIcon className='size-4 shrink-0' />,
+          description: err.message,
+        })
+      },
+    })
+  }
 
   return (
     <DropdownMenu
@@ -45,7 +78,52 @@ export function MyAccountDropdown({ user }: { user: Auth['user'] }) {
         align='end'
         className='min-w-[200px]'
       >
-        <DropdownMenuItem>
+        <DropdownMenuItem className='justify-between gap-4 focus:bg-transparent'>
+          <Avatar className='size-7 shrink-0'>
+            <AvatarImage src={user?.picture ?? undefined} />
+            <AvatarFallback className='leading-none bg-gray-3 text-sm'>
+              {user.name
+                .split(' ')
+                .map((s) => s[0]?.toUpperCase())
+                .join('')}
+            </AvatarFallback>
+          </Avatar>
+          <div className='flex flex-col gap-1'>
+            <span className='font-medium text-color-text-primary'>
+              {user.name}
+            </span>
+            <span className='text-color-text-quaternary'>{user.email}</span>
+          </div>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          className='group'
+          asChild
+        >
+          <Link href={`/dashboard`}>
+            <HouseIcon
+              strokeWidth={2.25}
+              className='size-4 shrink-0 group-hover:text-color-text-primary'
+            />
+            Dashboard
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          className='group'
+          asChild
+        >
+          <Link href={`/settings`}>
+            <SettingsIcon
+              strokeWidth={2.25}
+              className='size-4 shrink-0 group-hover:text-color-text-primary'
+            />
+            Preferences
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          className='group'
+          onClick={handleLogout}
+        >
           <LogOutIcon
             strokeWidth={2.25}
             className='size-4 shrink-0'
