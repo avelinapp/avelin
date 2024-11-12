68a32ae52588d3c9cc7ca2ae10d6cace2154dea0
Kian Bazarjani <kian@bazza.dev>
2024-11-12 16:38:23 -0500

    Trigger has hovered and "dropdown opened" states identical

--- apps/web/components/editor/my-account-dropdown.tsx
+++ apps/web/components/editor/my-account-dropdown.tsx
@@ -1,22 +1,35 @@
 import { Auth } from '@avelin/database'
+import { LogOutIcon } from '@avelin/icons'
 import { Avatar, AvatarImage, AvatarFallback } from '@avelin/ui/avatar'
 import { Button } from '@avelin/ui/button'
+import { cn } from '@avelin/ui/cn'
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@avelin/ui/dropdown-menu'
+import { useState } from 'react'
 
 export function MyAccountDropdown({ user }: { user: Auth['user'] }) {
+  const [open, setOpen] = useState(false)
+
   return (
-    <DropdownMenu>
+    <DropdownMenu
+      open={open}
+      onOpenChange={setOpen}
+    >
       <DropdownMenuTrigger asChild>
         <Button
           variant='ghost'
           className='h-fit w-fit p-0 relative rounded-full bg-none hover:bg-none group'
         >
-          <div className='z-10 absolute inset-0 rounded-full bg-gray-12/15 opacity-0 transition-opacity group-hover:opacity-100' />
+          <div
+            className={cn(
+              'z-10 absolute inset-0 rounded-full bg-gray-12/15 opacity-0 transition-opacity group-hover:opacity-100',
+              open && 'opacity-100',
+            )}
+          />
           <Avatar className='size-7 shrink-0'>
             <AvatarImage src={user?.picture ?? undefined} />
             <AvatarFallback className='leading-none bg-gray-3 text-sm'>
@@ -28,8 +41,17 @@ export function MyAccountDropdown({ user }: { user: Auth['user'] }) {
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align='end'>
-        <DropdownMenuItem>Log out</DropdownMenuItem>
+      <DropdownMenuContent
+        align='end'
+        className='min-w-[200px]'
+      >
+        <DropdownMenuItem>
+          <LogOutIcon
+            strokeWidth={2.25}
+            className='size-4 shrink-0'
+          />
+          Log out
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   )
