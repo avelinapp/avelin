c22be3cd37609e53a1d9d796d2df506e98751910
Kian Bazza <kian@bazza.dev>
2024-11-30 16:17:54 -0500

    fix: dark mode for login page (#173)
    Fix styling to use design system

--- apps/web/app/(auth)/login/page.tsx
+++ apps/web/app/(auth)/login/page.tsx
@@ -6,13 +6,13 @@ import { LoginWithGoogle } from './buttons'
 
 export default function Page() {
   return (
-    <div className='w-[400px] m-auto bg-white border border-neutral-100 rounded-xl shadow-lg flex flex-col gap-6 p-8 items-center'>
+    <div className='w-[400px] m-auto bg-popover-bg border border-color-border-subtle rounded-xl shadow-lg flex flex-col gap-6 p-8 items-center'>
       <LogoAvelin className='size-16' />
       <div className='space-y-4'>
         <h1 className='text-3xl font-semibold tracking-tight text-center'>
           Login to Avelin
         </h1>
-        <p className='text-center text-neutral-500'>
+        <p className='text-center text-color-text-quaternary'>
           Authenticate with your preferred provider.
         </p>
       </div>
