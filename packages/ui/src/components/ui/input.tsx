97a0842058b74f70564c1c5a37afa0a7c57ad9f1
Kian Bazarjani <kian@bazza.dev>
2024-12-12 22:33:35 -0500

    Working!

--- packages/ui/src/components/ui/input.tsx
+++ packages/ui/src/components/ui/input.tsx
@@ -4,7 +4,7 @@ import { cn } from '@avelin/ui/cn'
 import { cva, VariantProps } from 'class-variance-authority'
 
 const inputVariants = cva(
-  'flex w-full rounded-md border border-input bg-background text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+  'flex w-full rounded-md border border-color-border-subtle bg-popover-bg text-sm placeholder:text-color-text-primary/50 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
   {
     variants: {
       size: {
