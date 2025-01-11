7a0ebc5f57a33969df4d22083285ff666408a478
Kian Bazza <kianbazza@Kians-MacBook-Pro.local>
2025-01-11 17:12:35 -0500

    cd: print database URL during migrations for debugging

--- packages/database/src/migrate.ts
+++ packages/database/src/migrate.ts
@@ -1,4 +1,5 @@
 import { config } from 'dotenv'
+// import { sql } from 'drizzle-orm'
 import { drizzle } from 'drizzle-orm/neon-http'
 import { migrate } from 'drizzle-orm/neon-http/migrator'
 import path from 'path'
@@ -9,7 +10,15 @@ if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not set')
 }
 
+console.log('DATABASE_URL:', process.env.DATABASE_URL)
+
 const db = drizzle(process.env.DATABASE_URL)
+
+// Check migrations which have been already applied
+// Grabs rows from the "drizzle" schema in the "__drizzle_migrations" table
+// db.execute(sql`SELECT * FROM drizzle.__drizzle_migrations`).then((value) => {
+//   console.log(value.rows)
+// })
 
 console.log('Running migrations...')
 
