85ef97234aa29ea7cb54800243666c21924676c5
Kian Bazza <kianbazza@Kians-MacBook-Pro.local>
2025-01-25 07:48:01 -0500

    Update schema

--- packages/database/src/schema.ts
+++ packages/database/src/schema.ts
@@ -51,6 +51,7 @@ export const rooms = pgTable('rooms', {
   slug: text().unique(),
   ydoc: bytea(),
   title: text(),
+  editorLanguage: text().default('plaintext'),
   /* System user should be creator only for migrations and legacy data. */
   /* All rooms should be assigned a creator which is a real user. */
   creatorId: text().references(() => users.id, { onDelete: 'cascade' }),
