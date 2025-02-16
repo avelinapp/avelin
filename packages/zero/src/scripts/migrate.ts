1bbf8791b4022470ff88bc81d8d6b6eff9fe6d44
Kian Bazza <kianbazza@Kians-MacBook-Pro.local>
2025-02-16 12:43:54 -0500

    GH actions tasks for migrations are done

---
+++ packages/zero/src/scripts/migrate.ts
@@ -0,0 +1,52 @@
+import zeroConfig from '../../zero-config.json' assert { type: 'json' }
+
+import { config } from 'dotenv'
+import { sql } from 'drizzle-orm'
+import { drizzle } from 'drizzle-orm/neon-http'
+
+config({ path: '.env' })
+
+const db = drizzle(process.env.ZERO_UPSTREAM_DB!)
+
+console.log('======= ENV ========')
+console.log('ZERO_UPSTREAM_DB:', process.env.ZERO_UPSTREAM_DB)
+console.log('====================\n')
+
+console.log('======== ZERO CONFIG ========')
+console.log('Schema version:', zeroConfig.zero.schemaVersion)
+console.log('=============================\n')
+
+const data = await db.execute(sql`SELECT * FROM zero."schemaVersions"`).then(
+  (value) =>
+    value.rows[0] as {
+      minSupportedVersion: number
+      maxSupportedVersion: number
+      lock: boolean
+    },
+)
+
+console.log('======== DATABASE ========')
+console.log('Minimum supported version:', data.minSupportedVersion)
+console.log('Maximum supported version:', data.maxSupportedVersion)
+console.log('==========================\n')
+
+if (data.maxSupportedVersion < zeroConfig.zero.schemaVersion) {
+  console.log('Bumping maximum supported version...')
+  await db.execute(
+    sql`UPDATE zero."schemaVersions" SET "maxSupportedVersion" = ${zeroConfig.zero.schemaVersion}`,
+  )
+  const newData = (await db
+    .execute(sql`SELECT * FROM zero."schemaVersions"`)
+    .then((value) => value.rows[0])) as {
+    minSupportedVersion: number
+    maxSupportedVersion: number
+    lock: boolean
+  }
+
+  console.log(
+    'Maximum supported version bumped to:',
+    newData.maxSupportedVersion,
+  )
+} else {
+  console.log('Maximum supported version is up to date.')
+}
