06fb1cb21927e3df9a1c378a81f592730c88f15f
Kian Bazza <kianbazza@Kians-MacBook-Pro.local>
2025-02-16 22:51:08 -0500

    feat(devtools): dark mode

--- apps/web/components/devtools/toolbar.tsx
+++ apps/web/components/devtools/toolbar.tsx
@@ -27,20 +27,23 @@ export default function AvelinDevToolsToolbar() {
   console.log('Loading Avelin developer tools...')
 
   return (
-    <div className="z-10 bg-black h-9 flex items-center px-4 border border-color-border-subtle text-sm">
+    <div className="z-10 dark:bg-black bg-white h-9 flex items-center px-4 border border-color-border-subtle text-sm">
       <div className="flex items-center h-full gap-4">
         <LogoAvelin className="size-5" />
         <Button
-          className="h-full w-fit bg-transparent hover:bg-gray-2 rounded-none text-color-text-primary"
+          className={cn(
+            'h-full w-fit bg-transparent hover:bg-gray-3 rounded-none hover:text-color-text-primary',
+            FF_zero ? 'text-color-text-primary' : 'text-color-text-quaternary',
+          )}
           onClick={toggleZeroFeatureFlag}
         >
           <div
             className={cn(
               'rounded-full size-1.5',
-              FF_zero ? 'bg-green-10' : 'bg-gray-11',
+              FF_zero ? 'bg-green-10' : 'bg-red-10',
             )}
           />
-          Zero
+          <span>Zero</span>
         </Button>
       </div>
     </div>
