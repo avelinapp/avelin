5343fcd983aa96c821563022ac8d21494c1e7b48
Kian Bazza <kianbazza@Kians-MacBook-Pro.local>
2025-02-18 18:15:15 -0500

    Fix code room content height

--- apps/web/app/rooms/[slug]/layout.tsx
+++ apps/web/app/rooms/[slug]/layout.tsx
@@ -1,3 +1,5 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="light flex flex-col w-full flex-1">{children}</div>
+  return (
+    <div className="light flex flex-col w-full h-full flex-1">{children}</div>
+  )
 }
