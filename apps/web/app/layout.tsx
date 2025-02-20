a4b9266f9f9c5e26adc39f6e128504b2e0d1019a
Kian Bazza <kianbazza@Kians-MacBook-Pro.local>
2025-02-20 08:50:08 -0500

    Add one dollar stats, NODE_ENV var

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -4,9 +4,11 @@ import './globals.css'
 import '@avelin/ui/globals.css'
 import CommandMenu from '@/components/command-menu/command-menu'
 import AvelinDevToolsToolbar from '@/components/devtools/toolbar'
+import { env } from '@/lib/env'
 import Providers from '@/providers'
 import QueryClientProvider from '@/providers/query-client-provider'
 import { Toaster } from '@avelin/ui/sonner'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Avelin',
@@ -20,6 +22,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      {env.NODE_ENV === 'production' && (
+        <Script
+          defer
+          src="https://assets.onedollarstats.com/stonks.js"
+          id="stonks"
+          onLoad={() => console.log('Loaded One Dollar Stats')}
+        />
+      )}
       <body
         className={`${innovatorGrotesk.variable} ${berkeleyMono.variable} font-sans font-settings antialiased bg-color-background h-screen w-screen`}
       >
--- apps/web/lib/env.ts
+++ apps/web/lib/env.ts
@@ -13,6 +13,7 @@ export const env = createEnv({
 
   server: {
     DATABASE_URL: z.string().url().min(1),
+    NODE_ENV: z.enum(['development', 'production', 'test']),
   },
 
   /**
@@ -26,6 +27,7 @@ export const env = createEnv({
     NEXT_PUBLIC_ZERO_URL: process.env.NEXT_PUBLIC_ZERO_URL,
     NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
     DATABASE_URL: process.env.DATABASE_URL,
+    NODE_ENV: process.env.NODE_ENV,
   },
 
   /**
