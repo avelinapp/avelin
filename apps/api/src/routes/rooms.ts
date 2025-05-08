9bc381d1c6ab9c611d61bd76bb969fbf8949bb12
Kian Bazza <kian@bazza.dev>
2025-05-08 15:16:07 -0400

    feat(api): update hocuspocus connect and disconnect webhooks to use room_connections table

--- apps/api/src/routes/rooms.ts
+++ apps/api/src/routes/rooms.ts
@@ -1,6 +1,7 @@
 import { createHmac, timingSafeEqual } from 'node:crypto'
 import { auth, authCookies } from '@avelin/auth'
 import { type User, and, db, eq, schema, sql } from '@avelin/database'
+import { newId } from '@avelin/id'
 import { readableStreamToArrayBuffer } from 'bun'
 import Elysia from 'elysia'
 import { env } from '../env'
@@ -56,7 +57,8 @@ export const rooms = new Elysia({ prefix: '/rooms' }).guard({}, (app) =>
 
       const { event, payload } = data
       const roomId = payload.documentName as string
-      const serverId = payload.requestParameters.serverName as string
+      const serverId = payload.requestParameters.serverId as string
+      console.log('[/sync/webhook] event:', event, 'serverId:', serverId)
 
       switch (event) {
         /*
@@ -88,7 +90,7 @@ export const rooms = new Elysia({ prefix: '/rooms' }).guard({}, (app) =>
          * We use this to update the `room_participants` table.
          */
         case 'connect': {
-          console.log('connect event webhook')
+          console.log('[Sync] onConnect')
           const cookies = (payload.requestHeaders.cookie as string)
             .split('; ')
             .map((c) => {
@@ -128,38 +130,43 @@ export const rooms = new Elysia({ prefix: '/rooms' }).guard({}, (app) =>
           }
 
           const { user } = data
+          const roomConnectionId = newId('roomConnection')
+
+          console.log(
+            '[Sync] onConnect - user:',
+            user.id,
+            'serverId:',
+            serverId,
+          )
 
           try {
-            await db
-              .insert(schema.roomParticipants)
-              .values({
-                roomId: roomId,
-                userId: user.id,
-                isConnected: true,
-                connectedAt: new Date(),
-                connectionCount: 1,
-              })
-              .onConflictDoUpdate({
-                target: [
-                  schema.roomParticipants.roomId,
-                  schema.roomParticipants.userId,
-                ],
-                set: {
-                  connectionCount: sql`${schema.roomParticipants.connectionCount} + 1`,
+            await db.transaction(async (tx) => {
+              await tx
+                .insert(schema.roomParticipants)
+                .values({
+                  roomId: roomId,
+                  userId: user.id,
                   isConnected: true,
-                  connectedAt: new Date(),
                   lastAccessedAt: new Date(),
-                },
+                })
+                .onConflictDoNothing()
+              await tx.insert(schema.roomConnections).values({
+                id: roomConnectionId,
+                roomId,
+                userId: user.id,
+                serverId,
               })
+            })
           } catch (err) {
             console.error(
-              'Error on case CONNECT for sync webhook. Attempted to insert/update room_participants table',
+              'Error on case CONNECT for sync webhook. Attempted to insert room_participants & room_connections tables',
             )
             console.error(err)
           }
 
           return {
             serverId,
+            roomConnectionId,
           }
         }
 
@@ -169,33 +176,33 @@ export const rooms = new Elysia({ prefix: '/rooms' }).guard({}, (app) =>
          */
         case 'disconnect': {
           const user = payload.context.user as User
+          const roomConnectionId = payload.context.roomConnectionId as string
+
+          console.log(
+            '[Sync] onDisconnect - user:',
+            user.id,
+            'roomConnectionId:',
+            roomConnectionId,
+          )
 
           try {
             await db
-              .update(schema.roomParticipants)
+              .update(schema.roomConnections)
               .set({
-                /* We won't set `isConnected` to false, because we don't know if the user has other sessions. */
-                /* A database trigger will set `isConnected` to false if `connectionCount` is less than or equal to 0. */
-                /* We will set `connectionCount` to the current value minus 1. */
-                connectionCount: sql`${schema.roomParticipants.connectionCount} - 1`,
                 disconnectedAt: new Date(),
-                lastAccessedAt: new Date(),
+                isActive: false,
               })
-              .where(
-                and(
-                  eq(schema.roomParticipants.roomId, roomId),
-                  eq(schema.roomParticipants.userId, user.id),
-                ),
-              )
+              .where(eq(schema.roomConnections.id, roomConnectionId))
           } catch (err) {
             console.error(
-              'Error on case DISCONNECT for sync webhook. Attempted to update room_participants table',
+              'Error on case DISCONNECT for sync webhook. Attempted to update room_connections table',
             )
             console.error(err)
           }
 
           return {
             serverId,
+            roomConnectionId,
           }
         }
 
